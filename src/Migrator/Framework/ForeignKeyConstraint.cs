ffe341e9a1b10c72938d3d771c2d38a3851fce74
Ja bist du narrisch <No email sorry>
2025-06-25 16:20:47 +0200

    Added string id to FK

--- src/Migrator/Framework/ForeignKeyConstraint.cs
+++ src/Migrator/Framework/ForeignKeyConstraint.cs
@@ -2,23 +2,29 @@
 
 namespace DotNetProjects.Migrator.Framework;
 
-    public class ForeignKeyConstraint : IDbField
+public class ForeignKeyConstraint : IDbField
+{
+    public ForeignKeyConstraint()
+    { }
+
+    public ForeignKeyConstraint(string name, string table, string[] columns, string pkTable, string[] pkColumns, string stringId = null)
     {
-        public ForeignKeyConstraint()
-        { }
-
-        public ForeignKeyConstraint(string name, string table, string[] columns, string pkTable, string[] pkColumns)
-        {
-            Name = name;
-            Table = table;
-            Columns = columns;
-            PkTable = pkTable;
-            PkColumns = pkColumns;
-        }
-
-        public string Name { get; set; }
-        public string Table { get; set; }
-        public string[] Columns { get; set; }
-        public string PkTable { get; set; }
-        public string[] PkColumns { get; set; }        
+        StringId = stringId;
+        Name = name;
+        Table = table;
+        Columns = columns;
+        PkTable = pkTable;
+        PkColumns = pkColumns;
     }
+
+    /// <summary>
+    /// Gets or sets the Id of the FK. This is not the name of the FK.
+    /// SQLite: 
+    /// </summary>
+    public string StringId { get; set; }
+    public string Name { get; set; }
+    public string Table { get; set; }
+    public string[] Columns { get; set; }
+    public string PkTable { get; set; }
+    public string[] PkColumns { get; set; }
+}
