06377b2ec5045c34914694cd78637d2e588dfce5
Ja bist du narrisch <No email sorry>
2025-06-30 17:40:23 +0200

    AddTableTest

---
+++ src/Migrator.Tests/Providers/SQLite/SQLiteTransformationProvider_AddTableTests.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using DotNetProjects.Migrator.Framework;
+using DotNetProjects.Migrator.Providers.Impl.SQLite;
+using Migrator.Framework;
+using Migrator.Tests.Providers.SQLite.Base;
+using NUnit.Framework;
+
+namespace Migrator.Tests.Providers.SQLite;
+
+[TestFixture]
+[Category("SQLite")]
+public class SQLiteTransformationProvider_AddTableTests : SQLiteTransformationProviderTestBase
+{
+    [Test]
+    public void AddForeignKey()
+    {
+        var tableName = "MyTableName";
+        var columnName = "MyColumnName";
+
+        // Arrange/Act
+        _provider.AddTable(tableName, new Column(columnName, System.Data.DbType.Int32, ColumnProperty.Unique));
+
+        // Assert
+        var createScript = ((SQLiteTransformationProvider)_provider).GetSqlCreateTableScript(tableName);
+        Assert.That("CREATE TABLE MyTableName (MyColumn INTEGER UNIQUE)", Is.EqualTo(createScript));
+
+        var sqliteInfo = ((SQLiteTransformationProvider)_provider).GetSQLiteTableInfo(tableName);
+        Assert.That(sqliteInfo.Uniques.Single().KeyColumns.Single(), Is.EqualTo(columnName));
+    }
+}
