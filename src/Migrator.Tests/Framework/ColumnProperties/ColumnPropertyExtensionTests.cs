e17d8f344b5fbd57332a3e7f26941977670a7e9b
Ja bist du narrisch <No email sorry>
2025-07-30 15:22:35 +0200

    ColumnProperty extension tests

---
+++ src/Migrator.Tests/Framework/ColumnProperties/ColumnPropertyExtensionTests.cs
@@ -0,0 +1,91 @@
+using Migrator.Framework;
+using NUnit.Framework;
+using DotNetProjects.Migrator.Framework;
+using System;
+using System.Linq;
+
+namespace Migrator.Tests.Framework.ColumnProperties;
+
+public class ColumnPropertyExtensionsTests
+{
+    [Test]
+    public void Clear()
+    {
+        // Arrange
+        var columnProperty = ColumnProperty.PrimaryKey | ColumnProperty.NotNull;
+
+        // Act
+        columnProperty = columnProperty.Clear(ColumnProperty.PrimaryKey);
+
+        // Assert
+        Assert.That(columnProperty.HasFlag(ColumnProperty.PrimaryKey), Is.False);
+    }
+
+    [Test]
+    public void IsSet()
+    {
+        // Arrange
+        var columnProperty = ColumnProperty.PrimaryKeyWithIdentity | ColumnProperty.NotNull;
+
+        // Act
+        var actualData = GetAllSingleColumnProperties().Select(x => new
+        {
+            ColumnPropertyString = x.ToString(),
+            IsSet = columnProperty.IsSet(x),
+            IsNotSet = columnProperty.IsNotSet(x)
+        })
+        .ToList();
+
+        // Assert
+        string[] expectedSet = [nameof(ColumnProperty.PrimaryKey), nameof(ColumnProperty.NotNull), nameof(ColumnProperty.Identity)];
+        var actualDataShouldBeTrue = actualData.Where(x => expectedSet.Any(y => y == x.ColumnPropertyString)).ToList();
+        var actualDataShouldBeFalse = actualData.Where(x => !expectedSet.Any(y => y == x.ColumnPropertyString)).ToList();
+
+        Assert.That(actualDataShouldBeTrue.Select(x => x.IsSet), Has.All.True);
+        Assert.That(actualDataShouldBeFalse.Select(x => x.IsSet), Has.All.False);
+    }
+
+    [Test]
+    public void IsNotSet()
+    {
+        // Arrange
+        var columnProperty = ColumnProperty.PrimaryKeyWithIdentity | ColumnProperty.NotNull;
+
+        // Act
+        var actualData = GetAllSingleColumnProperties().Select(x => new
+        {
+            ColumnPropertyString = x.ToString(),
+            IsSet = columnProperty.IsNotSet(x),
+            IsNotSet = columnProperty.IsNotSet(x)
+        })
+        .ToList();
+
+        // Assert
+        string[] expectedSet = [nameof(ColumnProperty.PrimaryKey), nameof(ColumnProperty.NotNull), nameof(ColumnProperty.Identity)];
+        var actualDataShouldBeFalse = actualData.Where(x => expectedSet.Any(y => y == x.ColumnPropertyString)).ToList();
+        var actualDataShouldBeTrue = actualData.Where(x => !expectedSet.Any(y => y == x.ColumnPropertyString)).ToList();
+
+        Assert.That(actualDataShouldBeTrue.Select(x => x.IsNotSet), Has.All.True);
+        Assert.That(actualDataShouldBeFalse.Select(x => x.IsNotSet), Has.All.False);
+    }
+
+    [Test]
+    public void Set_Success()
+    {
+        // Arrange
+        var columnProperty = ColumnProperty.NotNull;
+
+        // Act
+        var result = columnProperty.Set(ColumnProperty.PrimaryKeyWithIdentity);
+
+        // Assert
+        var expected = ColumnProperty.NotNull | ColumnProperty.PrimaryKeyWithIdentity;
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    private ColumnProperty[] GetAllSingleColumnProperties()
+    {
+        return [.. Enum.GetValues<ColumnProperty>().Where(x => x == 0 || (x & (x - 1)) == 0)];
+    }
+}
--- src/Migrator/Framework/ColumnPropertyExtensions.cs
+++ src/Migrator/Framework/ColumnPropertyExtensions.cs
@@ -6,12 +6,12 @@ public static class ColumnPropertyExtensions
 {
     public static bool IsSet(this ColumnProperty columnProperty, ColumnProperty flags)
     {
-        return (columnProperty & flags) == flags;
+        return flags != ColumnProperty.None && (columnProperty & flags) == flags;
     }
 
     public static bool IsNotSet(this ColumnProperty columnProperty, ColumnProperty flags)
     {
-        return (columnProperty & (~flags)) == 0;
+        return (columnProperty & flags) == 0;
     }
 
     public static ColumnProperty Set(this ColumnProperty columnProperty, ColumnProperty flags)
