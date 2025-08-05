c1dd35745523083e735ec2550c125fe846ba8bfb
Ja bist du narrisch <No email sorry>
2025-08-05 09:26:40 +0200

    added create user script to  github actions

---
+++ .github/workflows/sql/oracle.sql
@@ -0,0 +1,16 @@
+alter session set container = freepdb1;
+create user k identified by k;
+grant
+   create user
+to k;
+grant
+   drop user
+to k;
+grant
+   create session
+to k with admin option;
+grant resource to k with admin option;
+grant connect to k with admin option;
+grant
+   unlimited tablespace
+to k with admin option;
