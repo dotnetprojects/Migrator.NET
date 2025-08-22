f9ff212715e7d05e3bf511f4eb8fed5bb469b139
JaBistDuNarrisch <Sorrynoemailaddress@sorrynoemailaddress.bla>
2025-08-22 11:39:25 +0200

    Added FilterType

---
+++ src/Migrator/Providers/Models/Indexes/Enums/FilterType.cs
@@ -0,0 +1,31 @@
+namespace DotNetProjects.Migrator.Providers.Models.Indexes.Enums;
+
+public enum FilterType
+{
+    None = 0,
+
+    /// <summary>
+    /// Greater than
+    /// </summary>
+    GreaterThan,
+
+    /// <summary>
+    /// Greater than or equal to
+    /// </summary>
+    GreaterThanOrEqualTo,
+
+    /// <summary>
+    /// Equal to
+    /// </summary>
+    EqualTo,
+
+    /// <summary>
+    /// Smaller than
+    /// </summary>
+    SmallerThan,
+
+    /// <summary>
+    /// Smaller than or equal to
+    /// </summary>
+    SmallerThanOrEqualTo
+}
