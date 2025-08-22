30cb42cae0a0857aa1e581cdcead1df86c93551f
JaBistDuNarrisch <Sorrynoemailaddress@sorrynoemailaddress.bla>
2025-08-22 13:53:56 +0200

    Oracle unique test

--- src/Migrator.Tests/Providers/OracleProvider/OracleTransformationProvider_AddIndexTests.cs
+++ src/Migrator.Tests/Providers/OracleProvider/OracleTransformationProvider_AddIndexTests.cs
@@ -2,7 +2,6 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using DotNetProjects.Migrator.Framework;
-using DotNetProjects.Migrator.Providers.Models.Indexes.Enums;
 using Migrator.Tests.Providers.Generic;
 using NUnit.Framework;
 using Oracle.ManagedDataAccess.Client;
@@ -19,5 +18,32 @@ public class OracleTransformationProvider_AddIndex_Tests : GenericAddIndexTestsB
         await BeginOracleTransactionAsync();
     }
 
+    [Test]
+    public void AddIndex_Unique_Success()
+    {
+        // Arrange
+        const string tableName = "TestTable";
+        const string columnName = "TestColumn";
+        const string columnName2 = "TestColumn2";
+        const string indexName = "TestIndexName";
 
+        Provider.AddTable(tableName, new Column(columnName, DbType.Int32), new Column(columnName2, DbType.String));
+
+        // Act
+        Provider.AddIndex(tableName,
+            new Index
+            {
+                Name = indexName,
+                KeyColumns = [columnName],
+                Unique = true,
+            });
+
+        // Assert
+        Provider.Insert(tableName, [columnName, columnName2], [1, "Hello"]);
+        var ex = Assert.Throws<OracleException>(() => Provider.Insert(tableName, [columnName, columnName2], [1, "Some other string"]));
+        var index = Provider.GetIndexes(tableName).Single();
+
+        Assert.That(index.Unique, Is.True);
+        Assert.That(ex.Number, Is.EqualTo(1));
+    }
 }
