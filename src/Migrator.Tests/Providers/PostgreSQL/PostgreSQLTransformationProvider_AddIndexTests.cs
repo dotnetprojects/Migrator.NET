08afa05152a42145f3f0bbf4a8cfcf7e7e4735df
JaBistDuNarrisch <Sorrynoemailaddress@sorrynoemailaddress.bla>
2025-08-27 15:38:54 +0200

    Minor changes

--- src/Migrator.Tests/Providers/PostgreSQL/PostgreSQLTransformationProvider_AddIndexTests.cs
+++ src/Migrator.Tests/Providers/PostgreSQL/PostgreSQLTransformationProvider_AddIndexTests.cs
@@ -165,9 +165,6 @@ public class PostgreSQLTransformationProvider_AddIndexTests : Generic_AddIndexTe
             .Using<string>((x, y) => string.Compare(x, y, ignoreCase: true)));
     }
 
-    /// <summary>
-    /// This test is located in the dedicated database type folder not in the base class since partial indexes (Oracle) are not supported in the migrator at this point in time.
-    /// </summary>
     [Test]
     public void AddIndex_FilteredIndexSingle_Success()
     {
